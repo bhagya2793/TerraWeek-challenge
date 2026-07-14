6d25cee5ad2dec14f9dd41f285acded8c152ebb6
bhagya2793 <bhagyapisal2793@gmail.com>
2026-07-14 06:18:29 +0000

    Completed Day01 Terraform hands-on implementation with local and random providers

--- Day01/example/main.tf
+++ Day01/example/main.tf
@@ -1,14 +1,33 @@
+# Day 1 Terraform Challenge
+# No cloud account or credentials required.
+# Generates a random pet name and creates a local greeting file.
+
 resource "random_pet" "name" {
-
-  length = 2
-
+  length    = 2
+  separator = "-"
 }
 
 
 resource "local_file" "greeting" {
+  filename = "${path.module}/greeting.txt"
 
-  filename = "greeting.txt"
+  content = <<-EOT
+Hello from TerraWeek 2026! 🚀
 
-  content = "Hello ${random_pet.name.id}, Welcome to TerraWeek Challenge!"
+Your infra pet name is: ${random_pet.name.id}
 
+Welcome to Terraform Week Challenge.
+EOT
+}
+
+
+output "pet_name" {
+  description = "The randomly generated infrastructure pet name."
+  value       = random_pet.name.id
+}
+
+
+output "file_path" {
+  description = "Location where the greeting file was created."
+  value       = local_file.greeting.filename
 }
--- Day01/example/terraform.tf
+++ Day01/example/terraform.tf
@@ -1,22 +1,19 @@
 terraform {
+  # Requires modern Terraform CLI
+  # Latest stable version at the time of writing: Terraform 1.15.x
+  required_version = ">= 1.10"
 
   required_providers {
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
 
     local = {
       source  = "hashicorp/local"
       version = "~> 2.5"
     }
 
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7"
+    }
+
   }
-
 }
-
-
-provider "random" {}
-
-provider "local" {}
